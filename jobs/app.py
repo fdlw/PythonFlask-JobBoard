0b8df334cd6f05fcce67148506c84bfa3895bd6f
GARRIDO Cédricq DGAC/Prestataires-Externe <fdlw.pro@gmail.com>
2020-04-17 15:18:04 +0200

    Update app.py

--- jobs/app.py
+++ jobs/app.py
@@ -1,5 +1,6 @@
 import sqlite3
-from flask import Flask, render_template, g
+import datetime
+from flask import Flask, render_template, g,  request, redirect, url_for
 PATH = 'db/jobs.sqlite'
 
 app = Flask(__name__)
