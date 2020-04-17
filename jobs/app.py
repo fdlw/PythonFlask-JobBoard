6a2debb5572407361c57a3669a4262b185a6d4f4
GARRIDO Cédricq DGAC/Prestataires-Externe <fdlw.pro@gmail.com>
2020-04-17 12:16:19 +0200

    Query Database Function Results

--- jobs/app.py
+++ jobs/app.py
@@ -14,6 +14,14 @@ def open_connection():
 
 def execute_sql(sql, values=(), commit=False, single=False):
     connection = open_connection()
+    cursor = connection.execute_sql(sql, values)
+    if commit == True:
+        results = connection.commit()
+    else:
+        results = cursor.fetchone() if single else cursor.fetchall()
+    cursor.close()
+    return results
+
 
 @app.route('/')
 @app.route('/jobs')
